e061ca2bc169bc22ae4610d15039c48deedcfc80
AndrÃ© Schloemp <andre.schloemp@prologa.de>
2016-02-24 07:03:46 +0000

    UI

--- webapp/controller/CreateContact.controller.js
+++ webapp/controller/CreateContact.controller.js
@@ -1,12 +1,23 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("FPE_CreateContact.controller.CreateContact", {
+		PEStepContainer: undefined,
+		onInit: function() {
+			var lPE = sap.ushell.Container.getService("ProcessEngine");
+			this.PEStepContainer = lPE.getCurrentStepContainer();
+			var lModel = new JSONModel({
+				CustomerName: this.PEStepContainer.CustomerName,
+				ContactText: this.PEStepContainer.ContactText
+			});
+			this.getView().setModel(lModel, "data");
+		},
 		onSubmit: function() {
-		    var lPE = sap.ushell.Container.getService("ProcessEngine");
-		    lPE.executeNext();
+			var lPE = sap.ushell.Container.getService("ProcessEngine");
+			lPE.executeNext();
 		}
 	});
 
